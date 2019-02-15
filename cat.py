75ce30c120786d1fd80637956669c9c6ea286d52
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-15 16:58:01 -0500

    Cats class complete

--- cat.py
+++ cat.py
@@ -4,14 +4,35 @@ class Cat:
     def __init__(self, name, pref_food, meal_time):
         self.name = name
         self.preffered_food = pref_food
-        self.meal_time = meal_time
+        self.meal_time = int(meal_time)
 
     def __str__(self):
-        return "Hi my name is {}, and I like to eat {} at {}- don't be late!".format(self.name, self.preffered_food, self.meal_time)
+        return "Cat name: {}\nMeal:{}\nMeal time:{}".format(self.name, self.preffered_food, self.meal_time)
+
+    def eats_at(self):
+        if self.meal_time > 12:
+            adjusted = self.meal_time - 12
+            return "{} PM".format(adjusted)
+        else:
+            return "{} AM".format(self.meal_time)
+
+    def meow(self):
+        return "Hi my name is {}, and I like to eat {} at {}- don't be late!".format(self.name, self.preffered_food, self.eats_at())
+
 
 
 james = Cat("James", "Tuna", "14")
 print(james)
 
 belle = Cat("Belle", "Salmon", "18")
-print(james)
+print(belle)
+
+
+
+print(belle.eats_at())
+
+statement = belle.meow()
+print(statement)
+
+statement2 = james.meow()
+print(statement2)
