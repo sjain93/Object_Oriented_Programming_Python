dbd1a814bcdf014d89ffb04b623b472f85001418
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-15 16:11:08 -0500

    created Cat class and str instance

---
+++ cat.py
@@ -0,0 +1,12 @@
+class Cat:
+    """Class about cats, enough said """
+
+    def __init__(self, name, pref_food, meal_time):
+        self.name = name
+        self.preffered_food = pref_food
+        self.meal_time = meal_time
+
+    def __str__(self):
+        return "Hi my name is {}, and I like to eat {} at {}- don't be late!".format(self.name, self.preffered_food, self.meal_time)
+
+    
