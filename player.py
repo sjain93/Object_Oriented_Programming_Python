0e6fd76f2733b4e09fd2a95217a2c18e4c245cce
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-17 18:11:06 -0500

    player game complete

--- player.py
+++ player.py
@@ -12,6 +12,34 @@ class player:
     def level_up(self):
         self.lives = self.lives + 1
 
+    def collect_treasure(self):
+        self.gold_coins = self.gold_coins + 1
+        if self.gold_coins%10 == 0:
+            self.level_up()
+
+    def restart():
+        self.gold_coins = 0
+        self.health_points = 10
+        self.lives= 5
+
+    def do_battle(self, damage):
+        self.health_points = self.health_points - int(damage)
+
+        if self.health_points<1:
+            if self.lives<1:
+                self.restart()
+            else:
+                self.lives = self.lives - 1
+                self.health_points = 10
 
 player1 = player()
+print(player1)
+
+player1.level_up()
+print(player1)
+
+player1.collect_treasure()
+print(player1)
+
+player1.do_battle(12)
 print(player1)
