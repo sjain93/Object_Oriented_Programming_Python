2dbb61e05c0e286c62a44536d5ae72fc3e4a2aa8
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-19 15:58:05 -0500

    Fixed class bug

--- paperboy/paperboy.py
+++ paperboy/paperboy.py
@@ -1,41 +1,44 @@
 class paperboy:
     """ A class representing a paperboy"""
-    min_papers = 50
 
     def __init__(self, name, experience, earnings):
         self.name = str(name)
         self.exp = float(experience)
         self.earnings= float(earnings)
+        self.quota = None
 
     def __str__(self):
         return "PaperBoy name:{}\nPaperBoy Experience:{}\nPaperBoy earnings:{}".format(self.name, self.exp, self.earnings)
 
-    def quota(self):
-        self.quot = float(min_papers + (self.exp/2))
-        return self.quot
+    def calculate_quota(self):
+        self.quota = float(50+ (self.exp/2))
 
     def deliver(self, start_address, end_address):
         self.start_address = int(start_address)
         self.end_address = int(end_address)
         self.papers_delivered = abs(self.end_address-self.start_address)
         self.exp = self.papers_delivered
-
-        if self.papers_delivered < quota.self.quot:
+        self.calculate_quota()
+        if self.papers_delivered < self.quota:
             self.earnings = float(self.papers_delivered*0.25 - 2)
-        elif self.papers_delivered == self.quot:
+        elif self.papers_delivered == self.quota:
             self.earnings = float(self.papers_delivered*0.25)
         else:
-            self.earnings = float(self.quot*0.25 + (self.papers_delivered-self.quot)*0.5)
+            self.earnings = float(self.quota*0.25 + (self.papers_delivered-self.quota*0.5))
 
         return self.earnings
 
     def report(self):
-        return "My name is {}, I've delivered {} papers and earned {} so far!".format(self.name, self.papers_delivered, self.earnings)
+        return "My name is {}, I've delivered {} papers and earned ${} so far!".format(self.name, self.papers_delivered, self.earnings)
 
 
 tommy = paperboy("Tommy", 0, 0)
-print(tommy)
+import pry;
+pry()
 
-tommy.deliver(101, 160)
-print(tommy.earnings)
-tommy.report()
+print(tommy)
+# print(tommy.quota())
+print(tommy.deliver(101, 160))
+
+print(tommy.report())
+# tommy.report()
