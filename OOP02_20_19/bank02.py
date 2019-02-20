6005ad685c4480865cf4b6b4084e295d3016c9c8
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-20 14:46:19 -0500

    Adding Feb 20 assignments, initial commit

---
+++ OOP02_20_19/bank02.py
@@ -0,0 +1,11 @@
+class BankAccont:
+    interest_rate = float()
+    #This is the prime interest rate
+    accounts = []
+    #All accounts, start as an empty list - to append to later
+    @classmethod
+    def total_funds(cls):
+        
+
+    def __init__(self, balance = 0):
+        self.acc_balance = balance
