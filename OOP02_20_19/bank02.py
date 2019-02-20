964aa314cbde49226409afb7c72d87c374faf60d
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-20 15:49:38 -0500

    Bank Account problem complete

--- OOP02_20_19/bank02.py
+++ OOP02_20_19/bank02.py
@@ -1,11 +1,58 @@
-class BankAccont:
-    interest_rate = float()
+class BankAccount:
+    interest_rate = float(1.0)
     #This is the prime interest rate
     accounts = []
     #All accounts, start as an empty list - to append to later
     @classmethod
+    def create(cls):
+        new_account = cls()
+        cls.accounts.append(new_account)
+        return new_account
+
+    @classmethod
     def total_funds(cls):
-        
+        total = 0
+        for account in cls.accounts:
+            total += account.acc_balance
+        return total
+
+    @classmethod
+    def interest_time(cls):
+        for account in cls.accounts:
+            account.acc_balance = account.acc_balance*(1+cls.interest_rate/100)
+
 
     def __init__(self, balance = 0):
         self.acc_balance = balance
+
+    def deposit(self, amount):
+        self.dep_amt= amount
+        self.acc_balance += self.dep_amt
+
+    def withdraw(self, amount):
+        self.with_amt= amount
+        self.acc_balance -= self.with_amt
+
+
+my_account = BankAccount.create()
+your_account = BankAccount.create()
+
+print(my_account.acc_balance)
+print(BankAccount.total_funds())
+
+my_account.deposit(200)
+your_account.deposit(1000)
+
+print(my_account.acc_balance)
+print(your_account.acc_balance)
+
+print(BankAccount.total_funds())
+BankAccount.interest_time()
+
+print(my_account.acc_balance)
+print(your_account.acc_balance)
+print(BankAccount.total_funds())
+
+my_account.withdraw(50)
+print(my_account.acc_balance)
+print(BankAccount.total_funds())
